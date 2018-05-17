9d86469e4e0e014aae8cb033eb9cf4b8349291a8
Behavior4 <geffenlab@gmail.com>
2018-05-17 17:24:03 -0400

    parameters

---
+++ projects/wheel_toneClouds/TC_booth2_lowFreq_params.m
@@ -0,0 +1,51 @@
+% a = logspace(log10(5000),log10(40000),8); % tone clouds
+a = log2space(5000, 15000,7); % tone clouds
+
+% stimulus info stuff
+stimInfo.stimFunction    = 'toneCloudGen'; % stimulus function
+stimInfo.tonePipDur      = 0.030;            % duration of each tone pip in cloud
+stimInfo.totalDur        = 1;                % total duration of each tone cloud
+stimInfo.cloudRange      = [a(1) a(3); a(5) a(7)]; % range of tones within the cloud
+stimInfo.nLogSteps       = 10;               % number of tones in the range
+stimInfo.envDur          = 0.005;            % duration of tone pip envelope
+stimInfo.tonePipRate     = 100;              % presentation rate in Hz (determines tone overlap)
+stimInfo.toneLevel       = 66;               % levels of tones in dB
+stimInfo.fs              = 192e3;
+
+% task specific stuff
+params.stimFunc = 'toneCloudGen_CA(stimInfo);';
+params.taskType = 'TCtrain_lowFreq';
+
+
+% booth specific parameters
+params.boothID = 'booth2';
+params.com = 'COM3';
+params.rewardDuration = 50;
+params.rotaryDebounce = 10;
+params.device = '2- Lynx E44';
+params.channel = [1 2];
+params.fs = 192e3;
+params.filtFile = 'booth2-170727-wdsfilter-192kHz';
+params.ampF = 10/11;
+
+% filter
+params.filtdir = 'D:\GitHub\filters';
+if ~exist(params.filtdir,'dir')
+    error('Filter directory not found, pull from GitHub.');
+end
+load([params.filtdir filesep params.filtFile]);
+params.filt = FILT;
+
+% task parameters
+params.holdDuration       = 1.5;
+params.respDuration       = 1.2;
+params.timeoutDuration    = 5000;
+params.trialTypeRatios    = [50 50];
+params.rewardContingency  = [2 1];
+params.timeOutContingency = [1 1];
+
+% fix some other things
+stimInfo.FILT = params.filt;
+stimInfo.respDuration = .5;
+
+
